15ec71b4d50f8c8d44e84c7345b86b1bef912b1c
s.yanada <ysthon@gmail.com>
2021-12-13 22:04:40 +0900

    skip authentication on graphql

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -1,4 +1,6 @@
 class GraphqlController < ApplicationController
+  skip_before_action :check_login
+  skip_before_action :create_request_payload
   # If accessing from outside this domain, nullify the session
   # This allows for outside API access while preventing CSRF attacks,
   # but you'll have to authenticate your user separately
