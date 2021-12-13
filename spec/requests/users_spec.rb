900e2a92ac416d4928a5c6050236ec3dedeac19d
msy7822 <msy780022@gmail.com>
2021-12-13 14:43:25 +0900

    rubocopのフォーマットを修正

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -4,13 +4,14 @@ RSpec.describe 'Users', type: :request do
   describe 'POST /users' do
     context '不正なJWTトークンが渡された場合' do
       before { post '/users?token=cjknsdjvhdsvjefhoviehve' }
+
       it '不正なトークンとして認識される' do
         json = JSON.parse(response.body)
         expect(json['error']).to eq('不正なトークンです。')
       end
 
       it 'スタータスコード401が返される' do
-        expect(response).to have_http_status(401)
+        expect(response).to have_http_status(:unauthorized)
       end
     end
   end
