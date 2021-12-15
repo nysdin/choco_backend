3e97440a323b26e7557486fc0d257fb8dbf87d28
s.yanada <ysthon@gmail.com>
2021-12-16 00:10:18 +0900

    idを追加

--- db/fixtures/01_user.rb
+++ db/fixtures/01_user.rb
@@ -1,5 +1,6 @@
 10.times do |n|
   User.seed do |s|
+    s.id = n + 1
     s.name = Faker::Name.name
     s.email = Faker::Internet.email(domain: 'google.com')
     s.nick_name = Faker::Internet.username
