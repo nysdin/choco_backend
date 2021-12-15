b1599e2d4827809edbccff62df173dedcfb715ed
msy7822 <msy780022@gmail.com>
2021-12-15 19:38:24 +0900

    Userモデルにバリデーションを追加

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -10,10 +10,9 @@ class GraphqlController < ApplicationController
     variables = prepare_variables(params[:variables])
     query = params[:query]
     operation_name = params[:operationName]
-    context = {
-      # Query context goes here, for example:
-      # current_user: current_user,
-    }
+    context = { current_user: @current_user }
+    auth_token = request.headers[:authorization]
+
     result = ChocoSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
     render json: result
   rescue => e
