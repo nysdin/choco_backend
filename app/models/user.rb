0c9f98f9e8f1da986eaa4891d30424b0f7137e68
s.yanada <ysthon@gmail.com>
2021-12-15 21:58:07 +0900

    User Merchandiseとの関連付けを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  has_many :listed_merchandises, class_name: 'Merchandise', foreign_key: 'seller_id'
+  has_many :purchased_merchandises, class_name: 'Merchandise', foreign_key: 'buyer_id'
 end
