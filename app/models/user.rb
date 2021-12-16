de44ca85fbe6e3c04db4da2493bc629c392f1e28
s.yanada <ysthon@gmail.com>
2021-12-16 20:58:39 +0900

    add dependent nullify

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :listed_merchandises, class_name: 'Merchandise', foreign_key: 'seller_id'
-  has_many :purchased_merchandises, class_name: 'Merchandise', foreign_key: 'buyer_id'
+  has_many :listed_merchandises, class_name: 'Merchandise', foreign_key: 'seller_id', dependent: :nullify
+  has_many :purchased_merchandises, class_name: 'Merchandise', foreign_key: 'buyer_id', dependent: :nullify
 
   validates :name, presence: true, length: { maximum: 255 }
   validates :email, presence: true, uniqueness: true, length: { maximum: 255 }
