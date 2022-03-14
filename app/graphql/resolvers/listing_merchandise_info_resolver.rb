35262dd711fe4474b061ea7bbaefed8c53e62231
msy7822 <msy780022@gmail.com>
2022-03-14 09:16:32 +0900

    不要な標準出力を削除

--- app/graphql/resolvers/listing_merchandise_info_resolver.rb
+++ app/graphql/resolvers/listing_merchandise_info_resolver.rb
@@ -4,8 +4,7 @@ module Resolvers
     argument :merchandise_id, Integer, required: true
 
     def resolve(merchandise_id:)
-      puts merchandise_id
-      pp Merchandise.eager_load(divided_department: :department).find_by(id: merchandise_id)
+      Merchandise.eager_load(divided_department: :department).find_by(id: merchandise_id)
     end
   end
 end
